5bfa1cb27066dd3069e97003aebe91f1a01d127a
Sachin Gupta <35871700+SachinGupta0206@users.noreply.github.com>
2025-06-03 01:34:46 +0530

    Create question.ts

---
+++ assets/question.ts
@@ -0,0 +1,33 @@
+import { Question } from '../types';
+
+export const quizQuestions: Question[] = [
+  {
+    question: 'The function which is called to render HTML to a web page in react?',
+    options: ['ReactDOM_render()', 'render()', 'render_DOM()', 'DOM_HTML()'],
+    correctAnswer: 'render()',
+  },
+  {
+   
+    question: 'What is Tailwind CSS?',
+    options: ['A CSS preprocessor like Sass','A utility-first CSS framework','A JavaScript library for styling','A set of pre-designed components' ],
+    correctAnswer: 'A utility-first CSS framework',
+  },
+  {
+  
+    question: 'Using which of the following command can prevent default behaviour at in react?',
+    options: ['preventDefault()', 'default()', 'avoidDefault()', 'None of the above'],
+    correctAnswer: 'preventDefault() ',
+  },
+  {
+    
+    question: 'Which class in Tailwind CSS makes text bold?',
+    options: ['bold', 'font-bold', 'strong', 'text-bold'],
+    correctAnswer: 'font-bold',
+  },
+  {
+   
+    question: 'Total ways of defining variables in ES6 is?',
+    options: ['1', '2', '3', '4'],
+    correctAnswer: '3',
+  },
+];
